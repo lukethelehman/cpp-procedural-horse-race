a43754b3d0590e35bc5c42a788a5bec8e8fee07b
lukethelehman <lukelehman05@gmail.com>
2025-01-31 10:51:18 -0500

    tested basic for loop and c++ syntax

--- horse-race.cpp
+++ horse-race.cpp
@@ -4,12 +4,17 @@ const int NUM_HORSES = 5;
 const int RACE_LENGTH = 15;
 
 int main(){
-	int horses[5] = {0,0,0,0,0};
+	int horses[5] = {1,2,3,4,5};
 	bool keepGoing = true;
 
-	while keepGoing {
+	while(keepGoing) {
 
-		for (int hn = 0; hn < 5; hn++){
+		for (int hn = 0; hn < NUM_HORSES; hn++){
+            std::cout << horses[hn] << std::endl;
+            }
+        keepGoing = false;
+        }
+    }
 							
 
 
