305b3c9e8bc270194b0a8be80ac4f6115de4d506
lukethelehman <lukelehman05@gmail.com>
2025-01-31 10:58:39 -0500

    added function shells

--- horse-race.cpp
+++ horse-race.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <random>
 
+std::random_device rd;
+std::uniform_int_distribution<int> dist(0,1)
 const int NUM_HORSES = 5;
 const int RACE_LENGTH = 15;
+void advance(int, int*);
+void printLine(int, int*);
+
 
 int main(){
 	int horses[5] = {1,2,3,4,5};
@@ -10,12 +16,21 @@ int main(){
 	while(keepGoing) {
 
 		for (int hn = 0; hn < NUM_HORSES; hn++){
-            std::cout << horses[hn] << std::endl;
+            advance(hn,horses);
+            printLine(hn,horses);
             }
         keepGoing = false;
         }
     }
-							
+
+
+void advance(int hn,int* horses){
+    std::cout << "test";
+    }
+
+void printLine(int hn, int* horses){
+    std::cout << "test";
+    }
 
 
 
