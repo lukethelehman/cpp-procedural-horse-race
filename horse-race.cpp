fe5a8dd2616c2918804bb271101388457e4e876e
lukethelehman <lukelehman05@gmail.com>
2025-01-31 15:37:16 -0500

    advance, print, and main functions working

--- horse-race.cpp
+++ horse-race.cpp
@@ -2,35 +2,49 @@
 #include <random>
 
 std::random_device rd;
-std::uniform_int_distribution<int> dist(0,1)
+std::uniform_int_distribution<int> dist(0,1);
 const int NUM_HORSES = 5;
 const int RACE_LENGTH = 15;
 void advance(int, int*);
-void printLine(int, int*);
+void printLane(int, int*);
 
 
 int main(){
-	int horses[5] = {1,2,3,4,5};
+	int horses[5] = {0,0,0,0,0};
 	bool keepGoing = true;
 
 	while(keepGoing) {
 
 		for (int hn = 0; hn < NUM_HORSES; hn++){
             advance(hn,horses);
-            printLine(hn,horses);
+            printLane(hn,horses);
             }
-        keepGoing = false;
+        std::cout << "Press ENTER for another round";
+        std::cin.ignore();        
         }
     }
 
 
 void advance(int hn,int* horses){
-    std::cout << "test";
+    int coin = dist(rd);
+    if (coin == 0){
+        horses[hn] ++;
     }
+}
 
-void printLine(int hn, int* horses){
-    std::cout << "test";
+void printLane(int hn, int* horses){
+    for (int i = 0; i < RACE_LENGTH; i++){
+        if (i == horses[hn]){
+            std::cout << hn;
+        }
+        
+        else {
+            std::cout << ".";
+        }
     }
+    std::cout << "" << std::endl;
 
+        
+}
 
 
